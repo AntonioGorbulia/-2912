5c08e87c0322906d80ddce16f21b857364a15277
AntonioGorbulia <123868143+AntonioGorbulia@users.noreply.github.com>
2023-01-31 17:48:10 +0100

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,16 @@
+class Cat:
+    def __init__(self, name, large, color):
+        self.name = name
+        self.large = large
+        self.color = color
+
+    def doing meow(self):
+        print(f"Cat {self.name} doing meow")
+
+     def go(self):
+        print(f"Cat {self.name} go")
+
+    c = ("Barsik", 100, "grey")
+
+    c.doing meow()
+    c.go()
